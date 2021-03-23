a3992e0cd772118d12541af08e03566e5eb40f26
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-03-23 16:40:48 +0100

    Begin to display brand in MainContent div

--- src/pages/KBeautyPage/KBeautyPage.jsx
+++ src/pages/KBeautyPage/KBeautyPage.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
 import { kBeautyActions } from "../../redux/k-beauty";
 import * as S from "./styled";
 
 const KBeautyPage = () => {
   const dispatch = useDispatch();
+  const [brand, setBrand] = useState();
+
+  const { brandslug } = useParams();
 
   const { brands, getAllBrandsError, loading } = useSelector(
     (state) => state.kBeauty
@@ -14,12 +18,53 @@ const KBeautyPage = () => {
     dispatch(kBeautyActions.getAllBrands());
     // eslint-disable-next-line
   }, []);
+  useEffect(() => {
+    if (brandslug) {
+      console.log(brandslug);
+      const theBrand = brands.find((branditem) => branditem.slug === brandslug);
+
+      if (theBrand) setBrand(theBrand);
+    } else {
+      setBrand(null);
+    }
+
+    // eslint-disable-next-line
+  }, [brandslug]);
 
   return (
     <S.Container>
-      <p>kBeautyPage</p>
+      <S.Sidebar>
+        <h2>What’s K-beauty?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam
+          condimentum tempus diam, ultricies sollicitudin erat facilisis eget.
+          Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam
+          condimentum tempus diam, ultricies sollicitudin erat facilisis eget.{" "}
+        </p>
 
-      {brands && brands.map((brand) => <p key={brands._id}>{brand.name}</p>)}
+        <ul>
+          {brands &&
+            brands.map((brand) => (
+              <li key={brands._id}>
+                <S.BrandLink to={`/k-beauty/${brand.slug}`}>
+                  {brand.name}
+                </S.BrandLink>
+              </li>
+            ))}
+        </ul>
+      </S.Sidebar>
+      <S.MainContent>
+        {brand && (
+          <S.BrandInfo>
+            <S.BrandName>{brand.name}</S.BrandName>
+            <S.Description>{brand.description}</S.Description>
+            <S.Resellers>
+              {brand.resellers.length > 0 &&
+                brand.resellers.map((reseller) => <li>{reseller}</li>)}
+            </S.Resellers>
+          </S.BrandInfo>
+        )}
+      </S.MainContent>
     </S.Container>
   );
 };
--- src/pages/KBeautyPage/styled.js
+++ src/pages/KBeautyPage/styled.js
@@ -1,3 +1,39 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
-export const Container = styled.div``;
+const activeClassName = "nav-item-active";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 50px;
+`;
+export const Sidebar = styled.div`
+  background-color: pink;
+`;
+
+export const BrandLink = styled(NavLink).attrs({ activeClassName })`
+  color: #b572f4;
+  text-decoration: none;
+
+  &.${activeClassName} {
+    font-weight: bold;
+    /* text-decoration: underline; */
+  }
+`;
+
+export const MainContent = styled.div`
+  background-color: lightblue;
+`;
+export const BrandInfo = styled.div``;
+
+export const BrandName = styled.h2`
+  font-family: "Playfair Display";
+  font-weight: 900;
+  font-size: 30px;
+  margin: 15px 0 0 0;
+  color: #e5bbcc;
+  line-height: 1;
+`;
+export const Description = styled.p``;
+export const Resellers = styled.ul``;
