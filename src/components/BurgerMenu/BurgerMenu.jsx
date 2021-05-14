23917742fda588ba7b4d44fe0af578c401bf6897
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-05-14 16:33:45 +0200

    Close BurgerMenu/sidenav when clicking a link

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -3,8 +3,35 @@ import * as S from "./styled";
 import { useDispatch, useSelector } from "react-redux";
 import { userTypes } from "../../redux/user";
 import Button from "../Button";
+import { appActions } from "../../redux/app";
 
-// import { decorator as reduxBurgerMenu } from "redux-burger-menu";
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    exact: true,
+  },
+  {
+    path: "/forum",
+    name: "Forum",
+    exact: false,
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    exact: false,
+  },
+  {
+    path: "/skinfluencers",
+    name: "Skinfluencers",
+    exact: true,
+  },
+  {
+    path: "/k-beauty",
+    name: "K-beauty",
+    exact: true,
+  },
+];
 
 const BurgerMenu = () => {
   const { isOpen } = useSelector((state) => state.app.burgerMenu);
@@ -15,20 +42,22 @@ const BurgerMenu = () => {
     dispatch({ type: userTypes.SIGN_OUT });
   };
 
+  const handleLinkClick = () => {
+    dispatch(appActions.setMenuIsOpen(false));
+  };
+
   return (
     <S.Menu isOpen={isOpen}>
       <S.Nav>
-        <S.NavLink to="/" exact>
-          Home
-        </S.NavLink>
-        <S.NavLink to="/forum">Forum</S.NavLink>
-        <S.NavLink to="/blog">Blog</S.NavLink>
-        <S.NavLink to="/skinfluencers" exact>
-          Skinfluencers
-        </S.NavLink>
-        <S.NavLink to="/k-beauty" exact>
-          K-beauty
-        </S.NavLink>
+        {routes.map((route) => (
+          <S.NavLink
+            to={route.path}
+            exact={route.exact}
+            onClick={handleLinkClick}
+          >
+            {route.name}
+          </S.NavLink>
+        ))}
       </S.Nav>
       {user && (
         <Button onClick={signOut} title="Sign Out" className="sign-out-btn" />
