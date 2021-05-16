6e01fb89686c6a1b2087c0ea50cbb20fbec97b1e
Matilda Söderblom <soderblom.matilda@gmail.com>
2021-05-16 14:57:54 +0200

    Styling for mobile & tablet

--- src/pages/HomePage/styled.js
+++ src/pages/HomePage/styled.js
@@ -5,23 +5,33 @@ import bgImagePortrait from "../../assets/images/tirza-van-dijk-dq0x8AvNKv8-unsp
 const glassStyles = css`
   background-color: rgba(255, 255, 255, 0.25);
   border-radius: 4px;
+  padding: 20px;
   @supports (backdrop-filter: blur(13px)) {
     background-color: rgba(255, 255, 255, 0.17);
     /* background-color: rgba(255, 255, 255, 0.3); */
     backdrop-filter: blur(13px);
     /* backdrop-filter: blur(20px); */
   }
+
+  @media (min-width: ${({ theme }) => theme.desktop}) {
+    padding: 20px 60px;
+  }
 `;
 
 export const Container = styled.div`
-  margin-top: -100px;
+  margin-top: -80px;
   background-image: url(${bgImagePortrait});
-  /* background-attachment: fixed; */
-  height: 100vh;
+  background-attachment: fixed;
+  /* height: 100vh; */
   background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
+  @media (min-width: ${({ theme }) => theme.tablet}) {
+    margin-top: -100px;
+    height: 100vh;
+    background-attachment: unset;
+  }
   @media (min-width: ${({ theme }) => theme.desktop}) {
     background-image: url(${bgImage});
   }
@@ -31,8 +41,8 @@ export const MainContent = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: auto auto auto;
   gap: 20px;
-  height: 70%;
-  width: 80%;
+  width: 90%;
+  padding: 100px 0 50px 0;
 
   p {
     padding: 5px 0;
@@ -46,14 +56,18 @@ export const MainContent = styled.div`
   }
 
   @media (min-width: ${({ theme }) => theme.tablet}) {
+    padding: unset;
+    height: 75%;
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 1fr 1fr;
     gap: 30px;
   }
   @media (min-width: ${({ theme }) => theme.desktop}) {
+    height: 70%;
     margin-top: 2%;
     height: 70%;
-    width: 70%;
+    /* width: 100%; */
+    max-width: 1200px;
   }
 `;
 
@@ -63,7 +77,6 @@ export const Info = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px 60px;
 
   @media (min-width: ${({ theme }) => theme.tablet}) {
     grid-column: 1/2;
@@ -76,7 +89,6 @@ export const Join = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px 60px;
 
   @media (min-width: ${({ theme }) => theme.tablet}) {
     grid-column: 2/3;
@@ -85,13 +97,14 @@ export const Join = styled.div`
 `;
 export const Latest = styled.div`
   ${glassStyles}
-  padding: 20px 60px;
+
   display: grid;
 
   grid-template-columns: 1fr auto;
   grid-template-rows: auto auto auto auto;
   h3 {
     grid-column: 1/3;
+    text-align: left;
   }
   p {
     text-align: left;
@@ -113,13 +126,13 @@ export const Title = styled.h3`
   font-weight: 900;
   font-size: 25px;
   margin: 0;
-  /* color: #b6a9e9; */
-  /* color: #e5bbcc; */
   color: white;
-  /* text-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1); */
+  text-align: center;
 
-  /* color: white; */
   @media (min-width: ${({ theme }) => theme.tablet}) {
+    font-size: 30px;
+  }
+  @media (min-width: ${({ theme }) => theme.desktop}) {
     font-size: 35px;
   }
 `;
